ea1a21aaf8800ee37114f79c7413b6c57ce07dd1
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-02-26 19:33:30 +0100

    Removing the click highjacking template.

--- WBIS/settings.py
+++ WBIS/settings.py
@@ -45,7 +45,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'WBIS.urls'
